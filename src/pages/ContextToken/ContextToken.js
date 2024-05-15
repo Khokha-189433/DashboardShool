1282a671aee58b82ba70c542670f8a5b91c5d460
Kh-Noufal <147395851+Khokha-189433@users.noreply.github.com>
2024-05-15 04:03:54 -0400

    add

--- src/pages/ContextToken/ContextToken.js
+++ src/pages/ContextToken/ContextToken.js
@@ -4,9 +4,9 @@ export const TokenAuth = createContext() ;
 export default function TokenProvider()
 {
     const [Auth, setAuth] = useState({});
-    return(<>
-       <TokenAuth.Provider value={{}}>
-          
-       </TokenAuth.Provider>
-    </>);
+    return (
+      <>
+         <TokenAuth.Provider value={{ Auth, setAuth }}> </TokenAuth.Provider>
+      </>
+    );
 }
