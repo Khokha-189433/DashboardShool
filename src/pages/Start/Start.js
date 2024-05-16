3305dff941b5168a8a7d962dd54795f341b9f6dc
Kh-Noufal <147395851+Khokha-189433@users.noreply.github.com>
2024-05-16 09:03:26 -0400

    haha

--- src/pages/Start/Start.js
+++ src/pages/Start/Start.js
@@ -2,9 +2,8 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const Start = () => {
-    if(localStorage.getItem("token"))
-        return <Navigate to="/home" />;
-    else return <Navigate to="/login" />;
+  if (localStorage.getItem("token")) return <Navigate to="/home" />;
+  else return <Navigate to="/login" />;
 };
 
 export default Start;
